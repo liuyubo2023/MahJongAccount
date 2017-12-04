a524b626e722eee122046f8e2b324ac6924f6b4b
yubo liu <liuyb@iclouddoor.com>
2017-12-04 21:53:49 +0800

    init

---
+++ MahJongAccount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MahJongAccount
+//
+//  Created by yubo liu on 2017/12/4.
+//  Copyright © 2017年 yubo liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
