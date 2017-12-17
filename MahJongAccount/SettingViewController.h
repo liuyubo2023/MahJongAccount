b93a947cd5303b10bb2faec154f34f000741b132
Michael <liukaiyi54@gmail.com>
2017-12-17 15:47:38 +0800

    修改姓名

--- MahJongAccount/SettingViewController.h
+++ MahJongAccount/SettingViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol SettingViewControllerDelegate
+
+- (void)sendNames:(NSArray *)names;
+
+@end
+
 @interface SettingViewController : UIViewController
+
+@property (nonatomic, weak) id <SettingViewControllerDelegate> delegate;
 
 @end
